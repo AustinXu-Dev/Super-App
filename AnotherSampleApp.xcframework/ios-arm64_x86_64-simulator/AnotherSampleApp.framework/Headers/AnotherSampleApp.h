ce517eebb400fa9195ed69218b40872eb1d304e7
Pyae Phyo Aung (Austin) <austin.xu.me@gmail.com>
2025-01-28 12:00:33 +0700

    fix bugs

---
+++ AnotherSampleApp.xcframework/ios-arm64_x86_64-simulator/AnotherSampleApp.framework/Headers/AnotherSampleApp.h
@@ -0,0 +1,18 @@
+//
+//  AnotherSampleApp.h
+//  AnotherSampleApp
+//
+//  Created by Austin Xu on 2567/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnotherSampleApp.
+FOUNDATION_EXPORT double AnotherSampleAppVersionNumber;
+
+//! Project version string for AnotherSampleApp.
+FOUNDATION_EXPORT const unsigned char AnotherSampleAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnotherSampleApp/PublicHeader.h>
+
+
--- SuperApp/ContentView.swift
+++ SuperApp/ContentView.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 import TodoMiniApp
+import AnotherSampleApp
 
 struct ContentView: View {
     
     var body: some View {
-        VStack {
+        TabView {
             TodoMiniAppView.createSampleView()
+                .tabItem {
+                    Label("Todo Mini", systemImage: "house.fill")
+                }
+            AnotherSampleAppView.createView()
+                .tabItem {
+                    Label("Another Sample", systemImage: "gear")
+                }
         }
         
     }
